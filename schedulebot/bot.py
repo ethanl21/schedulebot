ecbbf29098f1bef574429987816532ef9e9bfa45
Ethan L <15167713+gomihiko@users.noreply.github.com>
2021-08-28 20:42:46 -0700

    Update bot.py

--- schedulebot/bot.py
+++ schedulebot/bot.py
@@ -3,10 +3,15 @@ import discord.ext
 from discord_components import ComponentsBot
 import utils
 from schedule import ScheduleCog
+import os
 
 def main():
     # Get config values
-    token, prefix = utils.get_config()
+    if 'DYNO' in os.environ:
+        token = os.environ['token']
+        prefix = os.environ['prefix']
+    else:
+        token, prefix = utils.get_config()
 
     # Create bot object
     activity = discord.Game(name=f'{prefix}PLACEHOLDER')
